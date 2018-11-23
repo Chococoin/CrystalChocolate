1243c11561fb22c4929f57487bd964d268a10ef3
Germán Lugo <german.lugo@bitnibs.com>
2018-11-23 17:28:47 +0100

    Mockup code.

---
+++ config/mongodb_access_fake.js
@@ -0,0 +1,8 @@
+/*
+ * Example of code to connect the mongo_db
+ * This one is fake data 
+ */
+
+module.exports = {
+  mongoURI: 'mongodb://nameOfuser:VerySecurePassword@ds375321.mlab.com:55823/nameOfourDatabase'
+}
