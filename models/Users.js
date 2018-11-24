b1a32985f8c38e8cf4012280fb1d85547ef35b2e
Germán Lugo <german.lugo@bitnibs.com>
2018-11-24 18:00:32 +0100

    Improve schema.

--- models/Users.js
+++ models/Users.js
@@ -2,17 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	name: {
+	user: {
+		type: String,
+		require: true,
+	},
+	first_name: {
 		type: String,
 		require: true
+	},
+	last_name: {
+		type: String,
+		require: true,
 	},
 	password: {
 		type: String,
 		require: true
 	},
-	avatar: {
+	email: {
 		type: String,
 		require: true,
+	},
+	country: {
+		type: String,
+		require: true,
+	},
+	avatar: {
+		type: String,
+		require: false,
 	},
 	date: {
 		type: Date,
