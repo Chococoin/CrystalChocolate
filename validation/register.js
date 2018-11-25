21401e25a0d730e6bde605191dfa76fdb248cc8b
Germán Lugo <german.lugo@bitnibs.com>
2018-11-25 12:28:56 +0100

    Create validations register file.

---
+++ validation/register.js
@@ -0,0 +1,16 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty.js');
+
+function validateRegisterInput(data){
+  let errors = {};
+  if(!Validator.isLength(data, {min: 2, max: 30})){
+    errors.firstName = 'Name must be between 2 and 30 characters';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
+
+module.exports = validateRegisterInput;
