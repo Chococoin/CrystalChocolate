1aa5ffcee0cf93161b018f5d80f2e6f339235360
Germán Lugo <german.lugo@bitnibs.com>
2019-01-05 16:23:38 +0100

    Add random generator.

---
+++ helpers/random_status.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 12; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
