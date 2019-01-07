6cc316933b72dae5d6dd88d817d70b382b4cb265
Germán Lugo <german.lugo@bitnibs.com>
2019-01-07 17:27:48 +0100

    Add a Cookie hunter.

---
+++ helpers/whereIsMyChocoCookie.js
@@ -0,0 +1,21 @@
+var thereIsaChocoCookie = function(){
+  if (myChocoCookie !== null){
+    oauther.style.display = "none";
+    var data;
+    fetch('https://api.github.com/user?access_token='+ myChocoCookie)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    }).then( dat => {
+      data = dat;
+      console.log(data);
+      var img = document.createElement('IMG');
+      img.setAttribute("src", data.avatar_url);
+      img.setAttribute("width", "150");
+      img.setAttribute("height", "150");
+      document.getElementById('start').appendChild(img);
+    }).catch(error => console.log(error));
+  }
+}
+
+module.exports = thereIsaChocoCookie;
