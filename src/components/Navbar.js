0d9a088ba3bc8fb88d747cb735ca3276a9acded6
Germán Lugo <german.lugo@bitnibs.com>
2019-01-19 15:31:54 +0100

    Use ipcRenderer functions.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import '../../css/materialize.css'
 
-function Navbar(){
+const electron = window.require('electron')
+const ipcRenderer  = electron.ipcRenderer
+
+class Navbar extends React.Component{
+  signIn(e){
+    e.preventDefault();
+    ipcRenderer.send('signInMain:open')
+  }
+
+  signUp(e){
+    e.preventDefault();
+    ipcRenderer.send('signUpMain:open')
+  }
+
+  render(){
     return(
         <nav>
           <div className="nav-wrapper brown darken-3">
             <a className="logo"><img src="https://image.ibb.co/fm3oPf/logo.png" alt="logo" border="0"  style={{height: "75%", marginTop: "10px", marginLeft: "5px"}}/></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="" id="log" onclick="signIn()">Login</a></li>
-              <li><a href="" id="reg" onclick="signUp()">SignUp</a></li>
+              <li><a href="" id="log" onClick={this.signIn}>Login</a></li>
+              <li><a href="" id="reg" onClick={this.signUp}>SignUp</a></li>
             </ul>
           </div>
         </nav>
     )
+  }
 }
 
 export default Navbar
