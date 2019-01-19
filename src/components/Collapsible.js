f3e578a828672b849f09c8d3b440269bbbacfd34
Germán Lugo <german.lugo@bitnibs.com>
2019-01-19 14:38:16 +0100

    Fix Materialize into component.

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,50 +1,49 @@
 import React from 'react'
-import '../../css/materialize.css'
-//import { ipcRenderer } from 'electron'
+import M from '../../js/materialize.js'
+import LikeButton from './like_button'
 
+class Collapse extends React.Component{
 
-function Collapsible(){
+  componentDidMount(){
+    M.Collapsible.init(this.collapsible)
+  }
 
-  // function oauthGithub(e){
-  //   e.preventDefault();
-  //   ipcRenderer.send('OAuthGithub:open');
-  // }
-
-	return(
-          <ul className="collapsible" id="collaps">
-            <li>
-              <div className="collapsible-header valign-wrapper" id="inicio">inicio</div>
-              <div className="collapsible-body" id="start">
-                <a className="waves-effect waves-light btn-large social github" id="ghb" onClick={undefined}>
-
-                <i className="fa fa-github"></i> Sign in with github</a>
-              </div>
-            </li>
-            <li>
-              <div className="collapsible-header valign-wrapper" id="que">¿Qué es la chocosfera?</div>
-              <div className="collapsible-body"><div id="like_button_container"></div></div>
-            </li>
-            <li>
-              <div className="collapsible-header valign-wrapper" id="historia">historia</div>
-              <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
-            </li>
-            <li>
-              <div className="collapsible-header valign-wrapper" id="fabrica">Choco Fábrica</div>
-              <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
-            </li>
-            <li>
-              <div className="collapsible-header valign-wrapper" id="banco">Banco</div>
-              <div className="collapsible-body" id="banco-body"> 
-                <h3 className="col s4 center"> EUR </h3>
-                <p id="tag" className="col s4 center"><span>€ </span></p>
-              </div>
-            </li>
-            <li>
-              <div className="collapsible-header valign-wrapper" id="soporte">Soporte</div>
-              <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-            </li>
-          </ul>
-	)
+  render(){
+    return(
+            <ul className="collapsible" id="collaps" ref={(collapsible) =>  {this.collapsible = collapsible}}>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="inicio">inicio</div>
+                <div className="collapsible-body" id="start">
+                  <a className="waves-effect waves-light btn-large social github" id="ghb" onClick={undefined}>
+                  <i className="fa fa-github"></i> Sign in with github</a>
+                </div>
+              </li>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="que">¿Qué es la chocosfera?</div>
+                <div className="collapsible-body"><LikeButton /></div>
+              </li>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="historia">historia</div>
+                <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
+              </li>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="fabrica">Choco Fábrica</div>
+                <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
+              </li>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="banco">Banco</div>
+                <div className="collapsible-body" id="banco-body">
+                  <h3 className="col s4 center"> EUR </h3>
+                  <p id="tag" className="col s4 center"><span>€ </span></p>
+                </div>
+              </li>
+              <li>
+                <div className="collapsible-header valign-wrapper" id="soporte">Soporte</div>
+                <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+              </li>
+            </ul>
+    )
+  }
 }
 
-export default Collapsible
+export default Collapse
