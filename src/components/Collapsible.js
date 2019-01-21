e8acae34589dc2c406b5ea499c3a800d91383e64
Germán Lugo <german.lugo@bitnibs.com>
2019-01-21 13:20:22 +0100

    Fix indentation.

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -13,38 +13,38 @@ class Collapse extends React.Component{
 
   render(){
     return(
-            <ul className="collapsible" id="collaps" ref={(collapsible) =>  {this.collapsible = collapsible}}>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="inicio">inicio</div>
-                <div className="collapsible-body" id="start">
-                  <a className="waves-effect waves-light btn-large social github" id="ghb" onClick={undefined}>
-                  <i className="fa fa-github"></i> Sign in with github</a>
-                </div>
-              </li>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="que">¿Qué es la chocosfera?</div>
-                <div className="collapsible-body"><LikeButton /></div>
-              </li>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="historia">historia</div>
-                <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
-              </li>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="fabrica">Choco Fábrica</div>
-                <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
-              </li>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="banco">Banco</div>
-                <div className="collapsible-body" id="banco-body">
-                  <h3 className="col s4 center"> EUR </h3>
-                  <p id="tag" className="col s4 center"><span>€ </span></p>
-                </div>
-              </li>
-              <li>
-                <div className="collapsible-header valign-wrapper" id="soporte">Soporte</div>
-                <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-              </li>
-            </ul>
+      <ul className="collapsible" id="collaps" ref={(collapsible) =>  {this.collapsible = collapsible}}>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="inicio">inicio</div>
+          <div className="collapsible-body" id="start">
+            <a className="waves-effect waves-light btn-large social github" id="ghb" onClick={undefined}>
+            <i className="fa fa-github"></i> Sign in with github</a>
+          </div>
+        </li>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="que">¿Qué es la chocosfera?</div>
+          <div className="collapsible-body"><LikeButton /></div>
+        </li>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="historia">historia</div>
+          <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
+        </li>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="fabrica">Choco Fábrica</div>
+          <div className="collapsible-body valign-wrapper"><span>Lorem ipsum dolor sit amet.</span></div>
+        </li>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="banco">Banco</div>
+          <div className="collapsible-body" id="banco-body">
+            <h3 className="col s4 center"> EUR </h3>
+            <p id="tag" className="col s4 center"><span>€ </span></p>
+          </div>
+        </li>
+        <li>
+          <div className="collapsible-header valign-wrapper" id="soporte">Soporte</div>
+          <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+        </li>
+      </ul>
     )
   }
 }
