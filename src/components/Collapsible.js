c936477d9c96d011f34d4a70004c39a0c8d51161
Germán Lugo <german.lugo@bitnibs.com>
2019-02-17 17:01:21 +0100

    Fix typo.

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import M from '../../js/materialize.js'
 import LikeButton from './like_button'
-import Bankwheal from './Bankwheal'
+import Bankwheel from './Bankwheel'
 
 const electron = window.require('electron')
 const ipcRenderer  = electron.ipcRenderer
@@ -93,7 +93,7 @@ class Collapse extends React.Component{
           <div className="collapsible-header valign-wrapper" onClick={this.krakenRequest} id="banco">Banco</div>
           <div className="collapsible-body" id="banco-body">
             <h3 className="col s4 center"> EUR </h3>
-            <p className="col s4 center"> {this.state.founds === '' ? <Bankwheal /> : '€ ' + (this.state.founds) }</p>
+            <p className="col s4 center"> {this.state.founds === '' ? <Bankwheel /> : '€ ' + (this.state.founds) }</p>
           </div>
         </li>
         <li>
