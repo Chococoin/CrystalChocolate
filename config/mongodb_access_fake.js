61367da06e0c9e128b1befc8cc3c585a42f84c2a
Germån Lugo <german.lugo@bitnibs.com>
2019-02-22 15:39:49 +0100

    Give access to dummy db.

--- config/mongodb_access_fake.js
+++ config/mongodb_access_fake.js
@@ -1,9 +1,12 @@
 /*
  * Example of code to connect the mongo_db
- * This one is fake data
+ * This uncommented is fake data
+ * To do a quick test you may use the commented code below.
+ * Just copy and paste it to change the fake one. 
  *
  * module.exports = {
- *  mongoURI: 'mongodb://localhost:27017/crystalchocolate'
+ *  mongoURI: 'mongodb://test2019:2109test@ds155823.mlab.com:55823/crystalchocolate',
+ *  secretOrKey: 'secret_key'
  * }
  *
  */
