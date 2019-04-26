6eaead75965c7cb5ab1fb75f684fb183d8b5f1e8
Germán Lugo <german.lugo@bitnibs.com>
2019-04-26 09:19:44 +0200

    Add fake data access.

---
+++ config/mongodb_access.js
@@ -0,0 +1,17 @@
+/*
+ * Example of code to connect the mongo_db
+ * This uncommented is fake data
+ * To do a quick test you may use the commented code below.
+ * Just copy and paste it to change the fake one. 
+ *
+ * module.exports = {
+ *  mongoURI: 'mongodb://test2019:2109test@ds155823.mlab.com:55823/crystalchocolate',
+ *  secretOrKey: 'secret_key'
+ * }
+ *
+ */
+
+module.exports = {
+  mongoURI: 'mongodb://test2019:test1234>@ds155823.mlab.com:55823/crystalchocolate',
+  secretOrKey: 'secret_key'
+}
